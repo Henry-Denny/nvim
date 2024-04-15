266d7c99012caeb0beed532f17c842571e1ed1eb
Henry Denny <henry.denny12@gmail.com>
2024-04-15 14:28:11 +0100

    Format `theme.lua`.

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -1,35 +1,35 @@
 return {
-	{
-		'catppuccin/nvim',
-		name = 'catppuccin',
-		priority = 900,
-		config = function()
-			require('catppuccin').setup({
-				term_colors = true,
-				styles = {
-					comments = { "italic" },
-					conditionals = {},
-				},
-				integrations = {
-					treesitter = true,
-				},
-			})
-			vim.cmd.colorscheme 'catppuccin'
-		end,
-	},
-	{
-		-- Set lualine as statusline
-		'nvim-lualine/lualine.nvim',
-		-- See `:help lualine.txt`
-		opts = {
-			options = {
-				icons_enabled = true,
-				theme = 'auto',
-				component_separators = { left = '', right = '' },
-				section_separators = { left = '', right = '' },
-				globalstatus = true,
-			},
-			sections = { lualine_x = { 'filetype', 'tabnine' } }
-		},
-	}
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 900,
+    config = function()
+      require('catppuccin').setup {
+        term_colors = true,
+        styles = {
+          comments = { 'italic' },
+          conditionals = {},
+        },
+        integrations = {
+          treesitter = true,
+        },
+      }
+      vim.cmd.colorscheme 'catppuccin'
+    end,
+  },
+  {
+    -- Set lualine as statusline
+    'nvim-lualine/lualine.nvim',
+    -- See `:help lualine.txt`
+    opts = {
+      options = {
+        icons_enabled = true,
+        theme = 'auto',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
+        globalstatus = true,
+      },
+      sections = { lualine_x = { 'filetype', 'tabnine' } },
+    },
+  },
 }
