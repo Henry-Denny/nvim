4d60c84ea180a2a96cf6efa64799a8b30fb84b63
Henry Denny <henry.denny12@gmail.com>
2024-05-17 15:20:41 +0100

    Add Telescope Catppuccin colours.

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -14,6 +14,29 @@ return {
 
     telescope.load_extension 'file_browser'
 
+    local colors = require('catppuccin.palettes').get_palette()
+    local TelescopeColor = {
+      TelescopeMatching = { fg = colors.flamingo },
+      TelescopeSelection = { fg = colors.text, bg = colors.surface0, bold = true },
+
+      TelescopePromptTitle = { bg = colors.pink, fg = colors.mantle },
+      TelescopePromptPrefix = { bg = colors.surface0 },
+      TelescopePromptNormal = { bg = colors.surface0 },
+      TelescopePromptBorder = { bg = colors.surface0, fg = colors.surface0 },
+
+      TelescopePreviewTitle = { bg = colors.green, fg = colors.mantle },
+      TelescopePreviewNormal = { bg = colors.mantle },
+      TelescopePreviewBorder = { bg = colors.mantle, fg = colors.mantle },
+
+      TelescopeResultsTitle = { fg = colors.mantle },
+      TelescopeResultsNormal = { bg = colors.mantle },
+      TelescopeResultsBorder = { bg = colors.mantle, fg = colors.mantle },
+    }
+
+    for hl, col in pairs(TelescopeColor) do
+      vim.api.nvim_set_hl(0, hl, col)
+    end
+
     -- Keymaps
 
     -- vim.api.nvim_set_keymap(
