552cb9709733a7a65b69274f1beeae78a5bc9451
Henry Denny <henry.denny12@gmail.com>
2024-05-21 11:55:12 +0100

    Move `mini.surround` config into custom plugins directory.

---
+++ lua/custom/plugins/surround.lua
@@ -0,0 +1,20 @@
+return {
+  'echasnovski/mini.nvim',
+  config = function()
+    require('mini.surround').setup {
+      mappings = {
+        add = 'gza', -- Add surrounding in Normal and Visual modes
+        delete = 'gzd', -- Delete surrounding
+        find = 'gzf', -- Find surrounding (to the right)
+        find_left = 'gzF', -- Find surrounding (to the left)
+        highlight = 'gzh', -- Highlight surrounding
+        replace = 'gzr', -- Replace surrounding
+        update_n_lines = 'gzn', -- Update `n_lines`
+
+        suffix_last = 'l', -- Suffix to search with "prev" method
+        suffix_next = 'n', -- Suffix to search with "next" method
+      },
+      respect_scrolloff = true,
+    }
+  end,
+}
